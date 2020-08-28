bc93c2a303cbf2381c329bcee45f74e1df76ab80
Sir Rodriguez <dr.raw.phd@gmail.com>
2020-08-28 10:48:44 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Welcome from './components/welcome/Welcome'
+import Clock from './components/clock/Clock'
+import Contact from './components/contact/Contact'
+import Navigation from './components/navigation/Navigation'
+import './App.css'
+import { NotFound } from './components/NotFound'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Navigation/>
+
+        
+       
+        <Switch>
+          <Route exact
+          path="/" 
+          render={(props) => <Welcome {...props} name="Sir"/>}/>
+        
+
+          <Route exact
+          path="/welcome/:name" 
+          render={(props) => <Welcome {...props} name="Sir"/>}/>
+      
+          <Route path="/clock" component={Clock}/>
+     
+          <Route path="/contact" component={Contact}/>
+          
+          <Route component={NotFound}/>
+        </Switch>
+       
+
+          
+
+      </div>
+    )
+  }
 }
 
 export default App;
